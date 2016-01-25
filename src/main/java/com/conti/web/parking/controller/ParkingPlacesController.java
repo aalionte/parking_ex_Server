a892e34074a7659ab111f51a05858d5609a8cd6b
Ana Alionte <ana-maria.alionte@endava.com>
2016-01-25 22:56:02 +0200

    Add data real time

---
+++ src/main/java/com/conti/web/parking/controller/ParkingPlacesController.java
@@ -0,0 +1,20 @@
+package com.conti.web.parking.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.conti.web.parking.service.ParkingPlaceService;
+
+@RestController
+public class ParkingPlacesController {
+
+	@Autowired
+	ParkingPlaceService service;
+
+	@RequestMapping(value = "/parking/getFreePlaces", method = RequestMethod.GET)
+	public long parkingPlaceAction() {
+		return service.getTotal();
+	}
+}
